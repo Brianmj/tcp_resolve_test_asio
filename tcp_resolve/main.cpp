dfe3711e6a7a508d784f2509a8b8404fd064df97
brianmj <spamthiseml@hotmail.com>
2018-11-24 16:36:38 -0500

    Initial Commit

---
+++ tcp_resolve/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tcp_resolve
+//
+//  Created by Brian Jones on 11/24/18.
+//  Copyright © 2018 Brian Jones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
