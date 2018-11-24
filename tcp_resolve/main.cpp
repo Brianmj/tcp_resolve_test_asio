c6d4939a3adda8aa0c6b561ba69eac1f23ceb659
brianmj <spamthiseml@hotmail.com>
2018-11-24 18:14:15 -0500

    Some more comments

--- tcp_resolve/main.cpp
+++ tcp_resolve/main.cpp
@@ -15,17 +15,21 @@ namespace net = std::experimental::net;
 int main(int argc, const char * argv[]) {
     // insert code here...
     net::io_context ioc;
-    net::ip::tcp::resolver resolver{ioc};
     
+    net::ip::tcp::resolver resolver{ioc};   // create a resolver object that will be
+    // used for the query
+    
+    // specify the host and how we're going to connect
     std::string host("www.apple.com"), service("https");
     
     std::error_code ec;
     
-    auto ep = resolver.resolve(host, service, ec);
+    // try to query the host. If success, then it'll return a range of endpoints 
+    auto endpoints = resolver.resolve(host, service, ec);
     
     // if 0, then success
     if(!ec) {
-        for(auto &endpoint : ep) {
+        for(auto &endpoint : endpoints) {
             std::cout << endpoint.endpoint() << "\n";   // prints out apples ip address
             std::cout << endpoint.host_name() << "\n";  // prints out www.apple.com
             std::cout << endpoint.service_name() << "\n";   // used https, so it returns that
